f04097d63946ce87edc67aee269e2648d1957f39
Petrov Vyacheslav <vyacheslav.petrov@indels.org>
2025-10-17 15:21:43 +0900

    first commit

---
+++ Counter/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Петров Вячеслав on 17.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
